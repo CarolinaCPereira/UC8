fc5ea934edf289ef31a7cc2b54ac99fdacb23d3f
Ana Carolina <carolinacanp@hotmail.com>
2022-08-15 20:32:22 -0300

    versão final

--- Sistema.js
+++ Sistema.js
@@ -1,14 +1,16 @@
-let QuantidadeDeAlunos = 10;
+let ListaDeAlunos = ["Ana", "Rachel", "Fernanda", "Beth", "Verônica", "Yvone", "Mariana", "Claudia", "Alice", "Renata"];
+console.log (ListaDeAlunos.length)
 
-for (let index = 0; index < QuantidadeDeAlunos; index++) {
+for (let index = 0; index < ListaDeAlunos.length; index++) {
+    //const element = array[index]
     console.log(index)
     
 if (index == 0) {
-console.log("zero")
-}
-    
-if (index % 2 == 0) {
-    console.log("número par")
+console.log("zero localizado: " + ListaDeAlunos[index])
+} 
+
+else if (index % 2 == 0) {
+    console.log("número par: " + ListaDeAlunos[index])
  } 
  else
-    console.log ("número ímpar")}
+    console.log ("número ímpar: " + ListaDeAlunos[index])}
