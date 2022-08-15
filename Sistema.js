d373a2e96780e3232a99bebb85e75c13b9f6fe8a
Ana Carolina <carolinacanp@hotmail.com>
2022-08-15 20:03:02 -0300

    última alteração

---
+++ Sistema.js
@@ -0,0 +1,14 @@
+let QuantidadeDeAlunos = 10;
+
+for (let index = 0; index < QuantidadeDeAlunos; index++) {
+    console.log(index)
+    
+if (index == 0) {
+console.log("zero")
+}
+    
+if (index % 2 == 0) {
+    console.log("número par")
+ } 
+ else
+    console.log ("número ímpar")}
