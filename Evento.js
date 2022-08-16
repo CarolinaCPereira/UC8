15e0b28029904b513256d3f6fbcc1e5bd1f86811
Ana Carolina <carolinacanp@hotmail.com>
2022-08-15 21:08:35 -0300

    ultima alteração

--- Evento.js
+++ Evento.js
@@ -1,28 +1,26 @@
-const Today = new Date;
-DataDoEvento = new Date ("2022-08-02"); 
 
-console.log ("Inserir data do evento ano-mês-dia")
-console.log ("--------------------")
+let DataAtual = new Date (2022,8,15); 
+let DataDoEvento = new Date (2022,10,10)
+console.log (DataDoEvento)
 
-if (DataDoEvento > Today) {console.log ("cadastro permitido")}
-else {console.log ( "cadastro não permitido: data inválida")}
+if (DataDoEvento > DataAtual) {console.log ("cadastre o evento")}
+    else {console.log ( "cadastro não permitido: data inválida")}
 
 console.log ("--------------------")
 
 let IdadeDoParticipante = 11
-console.log ("Inserir idade do participante")
 
 if ( IdadeDoParticipante < 18) {console.log ("cadastro não permitido: idade mínima não alcançada")}
-else {console.log (" cadastro permitido")}
+    else {console.log (" cadastro permitido")}
+    console.log (IdadeDoParticipante + " anos")
 
 console.log ("--------------------")
 
 let listaDeParticipantes = ["Helena", "Fernanda", "Milena", "Mariana", "Maria", "Ana", "Leda", "Elaine", "Yvone", "Claudia", "Lina", "Alice"];
-console.log ("Inserir Lista de Participantes")
  let quantidadeDeParticipantes = listaDeParticipantes.length;
  
 if ( quantidadeDeParticipantes < 100 ) {console.log ("cadastro permitido")}
-else {console.log (" cadastro não permitido: excesso de participantes")}
+    else {console.log (" cadastro não permitido: excesso de participantes")}
 
 console.log ("--------------------")
 
